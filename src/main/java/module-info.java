38d2de533760248df17be239f67f318e5bf31c1a
Irina Ivlieva <ivlieva@haulmont.com>
2021-10-13 09:46:02 +0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module msa {
+    opens com.example.msa2021;
+
+    //compile dependencies
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+
+    //spring runtime introspection dependencies
+    requires spring.context;
+    requires spring.core;
+    requires spring.beans;
+
+    requires jdk.unsupported;//for netty low-level API access
+}
